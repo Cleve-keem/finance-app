ac722476a2c97b3c977ec8f429ff85774e98c4db
Cleve-keem <belloharyourmidey@gmail.com>
2025-06-03 11:34:25 -0500

    added an overlay

--- src/App.jsx
+++ src/App.jsx
@@ -27,21 +27,23 @@ function App() {
   }
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center relative bg-blue-100 overflow-hidden">
-      <div className={`text-center ${animation}`}>
-        <h1>{slides[step].title}</h1>
-        <p>{slides[step].desc}</p>
-      </div>
-      <div className="absolute bottom-0 p-7 w-full flex justify-between bg-zinc-50 shadow rounded-tl-4xl rounded-tr-4xl">
-        <button onClick={skip} className="text-blue-400">
-          Skip
-        </button>
-        <button
-          onClick={nextSlide}
-          className="bg-blue-500 text-zinc-50 rounded-[5px]"
-        >
-          Next
-        </button>
+    <div className="h-screen relative bg-blue-700 overflow-hidden">
+      <div className="h-full flex flex-col items-center justify-center bg-blue-200">
+        <div className={`text-center ${animation}`}>
+          <h1>{slides[step].title}</h1>
+          <p>{slides[step].desc}</p>
+        </div>
+        <div className="absolute bottom-0 p-7 w-full flex justify-between bg-zinc-50 shadow rounded-tl-4xl rounded-tr-4xl">
+          <button onClick={skip} className="text-blue-400">
+            Skip
+          </button>
+          <button
+            onClick={nextSlide}
+            className="bg-blue-500 text-zinc-50 rounded-[5px]"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
