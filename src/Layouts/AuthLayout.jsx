f54c91b50c634c1550d68dc76ecb7b3edb1c99b5
Cleve-keem <belloharyourmidey@gmail.com>
2025-06-05 19:33:19 -0500

    added slider to the outlet

--- src/Layouts/AuthLayout.jsx
+++ src/Layouts/AuthLayout.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { Link, Outlet, useLocation } from "react-router";
 import Socials from "../components/Socials";
 
 export default function AuthLayout() {
   const location = useLocation();
+  const [animation, setAnimation] = useState("");
   const isLoginPage = location.pathname.includes("/login");
+
+  let slider = document.getElementById("slider");
+  function animateTo(direction) {
+    slider.classList.add(
+      direction === "left" ? "slide-out" : "slide-out-right"
+    );
+
+    setTimeout(() => {
+      slider.classList.remove("slide-out", "slide-out-right");
+      slider.classList.add(
+        direction === "left" ? "slide-in" : "slide-in-right"
+      );
+
+      setTimeout(
+        () => slider.classList.remove("slide-in", "slide-in-right"),
+        500
+      );
+    }, 500);
+  }
+
   return (
     <div className="w-full px-8">
       <div className="w-[90%] mx-auto py-10 relative">
         {!isLoginPage && (
-          <Link to="/login" className="absolute text-xl">
+          <Link
+            onClick={() => animateTo("right")}
+            to="/login"
+            className="absolute text-xl"
+          >
             <FaArrowLeftLong />
           </Link>
         )}
@@ -26,13 +51,13 @@ export default function AuthLayout() {
         <p className="text-xl font-semibold">
           {isLoginPage ? "Login to your Account" : "Create your Account"}
         </p>
-        <div className="my-3">
+        <div className={`my-3 ${animation}`} id="slider">
           <Outlet />
         </div>
         <div className="flex flex-col gap-9 py-10">
           <div className="flex items-center justify-center gap-1 text-zinc-500">
-            <div className="w-3 h-[1px] bg-zinc-500" />
-            <p>
+            <div className="text-[12px] w-3 h-[1px] bg-zinc-500" />
+            <p className="text-inherit">
               Or <span>{isLoginPage ? "Sign in" : "Sign up"} </span>with
             </p>
             <div className="w-3 h-[1px] bg-zinc-500" />
@@ -42,7 +67,11 @@ export default function AuthLayout() {
         {isLoginPage && (
           <p className="text-center my-7">
             Don't have an account?
-            <Link to="/register" className="ml-1 text-blue-500 hover:underline">
+            <Link
+              onClick={() => animateTo("left")}
+              to="/register"
+              className="ml-1 text-blue-500 hover:underline"
+            >
               Sign up
             </Link>
           </p>
