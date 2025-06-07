22024830da55ce96e919ac700afee88ef006a7f6
Cleve-keem <belloharyourmidey@gmail.com>
2025-06-07 13:48:42 -0500

    added the toggle functionality

--- src/Layouts/AppLayout.jsx
+++ src/Layouts/AppLayout.jsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import { GrTransaction } from "react-icons/gr";
 import { IoHome } from "react-icons/io5";
 import { NavLink, Outlet } from "react-router";
 
 export default function AppLayout() {
+  const [isNavOpen, setIsNavOpen] = useState("-translate-x-full");
+
+  function openNav() {
+    setIsNavOpen((prev) =>
+      prev === "-translate-x-full" ? "" : "-translate-x-full"
+    );
+  }
+
   return (
     <div>
       <header className="flex items-center justify-between shadow-md px-2 mb-3">
         <div className="size-20">
           <img src="/finance.jpg" alt="finance logo" />
         </div>
-        <button className="flex flex-col gap-1">
+        <button onClick={openNav} className="flex flex-col gap-1">
           <div className="block w-8 h-1 bg-black" />
           <div className="block w-8 h-1 bg-black" />
           <div className="block w-8 h-1 bg-black" />
         </button>
-        <nav className="absolute top-0 left-0 w-[50%] bottom-0 bg-zinc-900 text-zinc-50 shadow-md">
+        <nav
+          className={`absolute top-0 left-0 w-[50%] bottom-0 bg-zinc-900 text-zinc-50 shadow-md ${isNavOpen}`}
+        >
           <h1 className="text-2xl font-bold ml-5 mt-5 mb-9">Finance</h1>
           <ul className="flex flex-col w-[85%]">
             <li className="py-3 hover:bg-amber-100 hover:text-zinc-900 hover:rounded-tr-2xl hover:rounded-br-2xl cursor-pointer">
