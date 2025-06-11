c8a5ad44044e989b2d5d9f7250cc334245bf8d50
Cleve-keem <belloharyourmidey@gmail.com>
2025-06-11 08:22:46 -0500

    fixed navbar position

--- src/Layouts/AppLayout.jsx
+++ src/Layouts/AppLayout.jsx
@@ -18,33 +18,35 @@ export default function AppLayout() {
   }
 
   return (
-    <div>
-      <header className="w-full flex items-center justify-between shadow-md px-2">
-        <div className="size-20">
-          <img src="/finance.jpg" alt="finance logo" />
+    <div className="h-screen">
+      <header className="fixed left-0 right-0 bg-zinc-50">
+        <div className="flex items-center justify-between shadow-md px-2">
+          <div className="size-20">
+            <img src="/finance.jpg" alt="finance logo" />
+          </div>
+          <button
+            onClick={openNav}
+            className="flex flex-col gap-1 relative w-8 h-8"
+          >
+            <div
+              className={`absolute w-8 h-1 bg-black origin-center transition-all duration-500 left-0 ${
+                isNavOpen ? "-rotate-45 top-3" : "top-0"
+              }`}
+            />
+            <div
+              className={`absolute w-8 h-1 bg-black transition-all duration-500 left-0 ${
+                isNavOpen ? "opacity-0" : ""
+              } `}
+            />
+            <div
+              className={`absolute block w-8 h-1 bg-black origin-center transition-all duration-500 left-0 ${
+                isNavOpen ? "rotate-45 top-3.5" : "top-4"
+              }`}
+            />
+          </button>
         </div>
-        <button
-          onClick={openNav}
-          className="flex flex-col gap-1 relative w-8 h-8"
-        >
-          <div
-            className={`absolute w-8 h-1 bg-black origin-center transition-all duration-500 left-0 ${
-              isNavOpen ? "-rotate-45 top-3" : "top-0"
-            }`}
-          />
-          <div
-            className={`absolute w-8 h-1 bg-black transition-all duration-500 left-0 ${
-              isNavOpen ? "opacity-0" : ""
-            } `}
-          />
-          <div
-            className={`absolute block w-8 h-1 bg-black origin-center transition-all duration-500 left-0 ${
-              isNavOpen ? "rotate-45 top-3.5" : "top-4"
-            }`}
-          />
-        </button>
         <nav
-          className={`absolute top-0 left-0 w-[50%] bottom-0 bg-zinc-900 text-zinc-50 shadow-md ${direction} transition-all duration-500`}
+          className={`absolute h-screen top-0 left-0 w-[50%] bg-zinc-900 text-zinc-50 shadow-md ${direction} transition-all duration-500`}
         >
           <h1 className="text-2xl font-bold ml-5 mt-5 mb-9">Finance</h1>
           <ul className="flex flex-col w-[85%]">
