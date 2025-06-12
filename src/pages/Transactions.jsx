3f0b4a8185a73c8096aae81f2f231ed4c6106004
Cleve-keem <belloharyourmidey@gmail.com>
2025-06-12 02:38:01 -0500

    restructured the search layout1

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -122,7 +122,7 @@ export default function Transactions() {
               <input
                 type="text"
                 placeholder="Search Transaction"
-                className="py-2 px-3 outline-none flex-1"
+                className="py-2 px-3 outline-none flex-1 shrink"
               />
               <button className="min-w-10 button border border-zinc-200">
                 S
